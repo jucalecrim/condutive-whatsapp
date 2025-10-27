350712a3ae109dfcd4d89b4749c3aa6278444ee3
jucalecrim <jucalecrim@outlook.com>
2025-10-27 16:25:30 -0300

    missing psycopg2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     version=get_version(),
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    install_requires=['fastapi', 'numpy', 'pandas', 'datetime', 'flask', 'gunicorn', 'uvicorn', 'requests', 'validate_docbr'],
+    install_requires=['fastapi', 'numpy', 'pandas', 'datetime', 'flask', 'gunicorn', 'uvicorn', 'requests', 'validate_docbr', 'psycopg2'],
     description="Pacote de formulas para API do bot de whatsapp",
     author="jucalecrim",
     author_email="jucalecrim@outlook.com",
