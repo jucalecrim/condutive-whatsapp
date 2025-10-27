c7de2c012323495844e20a44e64ea41f6b75c3ec
jucalecrim <jucalecrim@outlook.com>
2025-10-27 09:32:18 -0300

    ok captcha

--- src/condutive_whatsapp/main.py
+++ src/condutive_whatsapp/main.py
@@ -2,7 +2,7 @@
 from fastapi import FastAPI, Header, HTTPException, Query, Request, Body
 from fastapi.responses import HTMLResponse, JSONResponse
 from typing import Optional
-#import pacote_back_condutive as pk
+import pacote_back_condutive as pk
 from condutive_whatsapp.core import hello_world
 
 
@@ -12,7 +12,11 @@ app = FastAPI()
 def read_root():
     return """O repo está funcionando"""
 
-@app.post("/test")
+@app.get("/test")
 def funct():
     return hello_world()
+
+@app.get("/captcha")
+def get_captcha():
+    return pk.charada()
 
